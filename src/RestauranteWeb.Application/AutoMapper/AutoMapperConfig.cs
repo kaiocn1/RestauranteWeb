0ab1cda87e627503f83ccc5cb5c48010d0cedc00
Kaio Nascimento <kaio@actuar.group>
2018-09-26 16:00:09 -0300

    Adicionando testes de integração.

--- src/RestauranteWeb.Application/AutoMapper/AutoMapperConfig.cs
+++ src/RestauranteWeb.Application/AutoMapper/AutoMapperConfig.cs
@@ -7,10 +7,11 @@ namespace RestauranteWeb.Application.AutoMapper
         public static void RegisterMappings()
         {
             Mapper.Initialize(x =>
-                              {
-                                  x.AddProfile<DomainToViewModelMappingProfile>();
-                                  x.AddProfile<ViewModelToDomainMappingProfile>();
-                              });
+                   {
+                       x.AddProfile<DomainToViewModelMappingProfile>();
+                       x.AddProfile<ViewModelToDomainMappingProfile>();
+                   });
+
         }
     }
 }
