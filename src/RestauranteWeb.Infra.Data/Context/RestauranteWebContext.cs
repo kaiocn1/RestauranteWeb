b4798399698b0d501593b2e1a3649294fac292df
Kaio Nascimento <kaio@actuar.group>
2018-09-26 10:46:23 -0300

    Ajustando contexto

--- src/RestauranteWeb.Infra.Data/Context/RestauranteWebContext.cs
+++ src/RestauranteWeb.Infra.Data/Context/RestauranteWebContext.cs
@@ -4,6 +4,8 @@ using RestauranteWeb.CrossCutting.Utils;
 using RestauranteWeb.Infra.Data.Configurations;
 using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RestauranteWeb.Infra.Data.Context
 {
@@ -18,20 +20,40 @@ namespace RestauranteWeb.Infra.Data.Context
 
         public override int SaveChanges()
         {
-            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataHoraCadastro") != null))
             {
                 if (entry.State == EntityState.Added)
                 {
-                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                    entry.Property("DataHoraCadastro").CurrentValue = DateTime.Now;
                 }
 
                 if (entry.State == EntityState.Modified)
                 {
-                    entry.Property("DataCadastro").IsModified = false;
+                    entry.Property("DataHoraCadastro").IsModified = false;
                 }
             }
 
             return base.SaveChanges();
+        }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataHoraCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataHoraCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataHoraCadastro").IsModified = false;
+                }
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         private string GetTableName(Type type)
